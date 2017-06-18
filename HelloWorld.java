affc56013843b74430287593812c0a77df2276c2
root <root@infra.emboss-local.com>
2017-06-18 13:17:43 -0400

    11

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+public static void main(String[] args) {  
+    for(int i=1;i<=10;i++){  
+        System.out.println(i);  
+        System.out.println("MY Name is KHAN");  
+    }  
+} 
+}
